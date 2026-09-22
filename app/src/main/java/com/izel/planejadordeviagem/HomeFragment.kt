c7baa574cedebac28c67873894074e60c83d1266
enrique-izel-sns <eleaoizel@gmail.com>
2026-09-21 22:39:12 -0400

    feat: add viewBinding and create navigation to distanceInputFragment

--- app/src/main/java/com/izel/planejadordeviagem/HomeFragment.kt
+++ app/src/main/java/com/izel/planejadordeviagem/HomeFragment.kt
@@ -5,15 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.izel.planejadordeviagem.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        _binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.startButton.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_distanceInputFragment2)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
