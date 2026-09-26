a29c70859663247e3f637efa986adc0cc647bfd1
enrique-izel-sns <eleaoizel@gmail.com>
2026-09-25 22:11:48 -0400

    feat: implement viewModel in order to calculate and show the result

--- app/src/main/java/com/izel/planejadordeviagem/TripCostResultFragment.kt
+++ app/src/main/java/com/izel/planejadordeviagem/TripCostResultFragment.kt
@@ -1,16 +1,18 @@
 package com.izel.planejadordeviagem
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.izel.planejadordeviagem.databinding.FragmentTripCostResultBinding
 
 class TripCostResultFragment : Fragment() {
     private var _binding: FragmentTripCostResultBinding? = null
     private val binding get() = _binding!!
+    private val viewModel: TripCalculatorViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,13 +25,25 @@ class TripCostResultFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val fuelConsumption = viewModel.fuelConsumption
+        val fuelPrice = viewModel.fuelPrice
+        val tripCostEstimatedResult = viewModel.calculateTripCost()
+
+        with(binding) {
+            averageConsumptionResult.text = getString(R.string.consumption_label, fuelConsumption)
+            fuelPriceResult.text = getString(R.string.fuel_price_label, fuelPrice)
+            estimatedCostResult.text = getString(R.string.total_cost_label, tripCostEstimatedResult)
+        }
+
         binding.newTripButton.setOnClickListener {
-            findNavController().navigate(R.id.action_homeFragment_to_distanceInputFragment)
+            viewModel.clearData()
+
+            findNavController().navigate(R.id.action_tripCostResultFragment_to_homeFragment)
         }
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
+    override fun onDestroyView() {
+        super.onDestroyView()
         _binding = null
     }
 }
